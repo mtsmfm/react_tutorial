d9a104f1fabb7a399b1b048a451b8fe052230f76
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-01-27 23:21:44 +0900

    change config

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,16 @@ module ReactTutorial
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.sass.preferred_syntax = :sass
+
+    config.generators do |g|
+      g.stylesheets         false
+      g.javascripts         false
+      g.helper              false
+      g.template_engine     false
+      g.test_framework      false
+      g.fixture_replacement false
+    end
   end
 end
